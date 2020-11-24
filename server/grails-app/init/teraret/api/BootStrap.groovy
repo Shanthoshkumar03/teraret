1e02e1833ac796be0431b94bba9bc17029d786cd
Harish Babu B K <babuamuda@gmail.com>
2020-11-24 12:56:11 +0530

    Created a Basic Application

---
+++ server/grails-app/init/teraret/api/BootStrap.groovy
@@ -0,0 +1,9 @@
+package teraret.api
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ server/src/main/webapp/precache-manifest.dcc799049406e1a341dd7a2b68806ce4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194142718936388e150b83b68feaced8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6727196ff24f2879c5e4",
+    "url": "/static/css/2.eb1025ee.chunk.css"
+  },
+  {
+    "revision": "2c167bfad67d05a3e96e",
+    "url": "/static/css/main.dc7fa479.chunk.css"
+  },
+  {
+    "revision": "6727196ff24f2879c5e4",
+    "url": "/static/js/2.be01f187.chunk.js"
+  },
+  {
+    "revision": "1d9ed9265a32ea4776472f445cf2df78",
+    "url": "/static/js/2.be01f187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c167bfad67d05a3e96e",
+    "url": "/static/js/main.c0f88c5d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "06b528621d135871fdd9cdc988916970",
+    "url": "/static/media/advancedgrails.06b52862.svg"
+  },
+  {
+    "revision": "e1490625d159ac8303c1cace0bad1904",
+    "url": "/static/media/documentation.e1490625.svg"
+  },
+  {
+    "revision": "b651640f09f462efc43058482f4131d4",
+    "url": "/static/media/grails-cupsonly-logo-white.b651640f.svg"
+  },
+  {
+    "revision": "cda5b2716e249b1f09558c5c3aa79ddb",
+    "url": "/static/media/grails.cda5b271.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "07688f36a61209ddace7da11c5eabdd4",
+    "url": "/static/media/slack.07688f36.svg"
+  }
+]);
